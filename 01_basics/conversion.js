bd044d7824db4b16d87247186617a2b287cb3c1e
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-12 22:05:47 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,20 @@
+let data="33ab"
+
+// it will print string
+console.log(typeof data);
+// it will print string as well because both methods are same
+console.log(typeof(data));
+
+let value=Number(data)
+// it will print number
+console.log(typeof value);
+// it will print NaN which is not a number 
+console.log(value)
+
+// value coversion
+// "33"=>string
+// 33 =>number
+// true=>1
+// false=>0
+// null=0
+// undefined=NaN

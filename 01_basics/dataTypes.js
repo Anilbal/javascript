9046eec221c98149954440fe8fb933f7490ce39e
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-12 14:33:46 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// there are two types of data types
+/* 1) Primitive 
+    a.String="",'' & ``
+    b.Number=1,2,3 etc.
+    c.Boolean =true or false
+    d.Null=standalone value
+    e.Undefined= variable declared but does assign value
+    f.Symbol=@,#
+*/
+
+/*
+ 2)Non primitive
+  a.Object
+*/
+
+
+console.log(typeof undefined)
+console.log(typeof null)
+
+// while consoling typeOf undefined it shows undefined but typeOf null shows Object as a type

491ca8afe981d94d6b40a4232789e7312b5e40e7
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-14 13:24:22 +0000

    operation

---
+++ 01_basics/operation.js
@@ -0,0 +1,17 @@
+let str1="10"
+let str2="5"
+
+console.log(str1+str2)
+// output will be 105
+
+console.log(10+5)
+// output will be 15
+
+console.log("10"+5+5)
+// output will be 1055 because in js while doing operation if first variable is str then it considered all variable as string
+
+console.log(10+5+"5")
+// output will be 155 because first two is number but after that is string 
+
+
+// study postfix and prefix in js mdn

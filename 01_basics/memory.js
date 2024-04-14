d881f39d64ca2fdbf414ad9e51f83ed9057f8c7e
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-14 18:40:31 +0000

    memory

--- 01_basics/memory.js
+++ 01_basics/memory.js
@@ -1,5 +1,5 @@
 /*
 in js, data are stored in two places i.e stack and heap
- => stack is used for storing premitivie types
- => heap is used for storing non primitive data types
+ => stack is used for storing premitivie types : in stack while taking value or variable ,copy of that variable or value  is provided not original
+ => heap is used for storing non primitive data types :in heap while taking value or variable ,original  of that variable or value  is provided 
 */

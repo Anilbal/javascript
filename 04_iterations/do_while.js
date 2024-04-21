78601bb154e80feda06aef0c2afbacf5b010cf39
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-21 12:10:32 +0000

    do while loop in js

---
+++ 04_iterations/do_while.js
@@ -0,0 +1,16 @@
+//do while loop in js
+/*
+SYNTAX:
+initialization
+do{
+    increment or decrement 
+}while(condition);
+*/
+
+
+let i=1
+do{
+    console.log(` Value is ${i}`)
+    i++
+}while(i<10)
+//in do while whatever the value is declared at first it is printed directly at first then after condition is checked

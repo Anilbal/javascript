d2d54d6fbd744404865e46d2d1e69dd9ae483278
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-21 12:05:37 +0000

    while loop in js

---
+++ 04_iterations/while.js
@@ -0,0 +1,23 @@
+//while loop 
+/*
+SYNTAX:
+initialization
+while(condition){
+    increment or decrement
+}
+*/
+// let i=1
+// while(i<=10){
+//     console.log(i)
+//     // i++
+//     i+=2
+// }
+
+
+//while with array
+const myArray=['1',"2","3"]
+let i=0
+while(i<myArray.length){
+    console.log(`Value is : ${myArray[i]}`)
+    i++
+}

b578f865f26b3961827a625b2b4d4f7724291c19
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-28 11:09:49 +0000

    xml request api

---
+++ 08_api/script.js
@@ -0,0 +1,15 @@
+//XML HttpRequest =>it is said to be ajax request
+/*
+0	UNSENT	Client has been created. open() not called yet.
+1	OPENED	open() has been called.
+2	HEADERS_RECEIVED	send() has been called, and headers and status are available.
+3	LOADING	Downloading; responseText holds partial data.
+4	DONE	The operation is complete.
+*/
+
+//Ajax (Asynchronous JavaScript and XML) programming is a technique used in web development to create interactive web 
+//applications. It allows you to send and receive data asynchronously between the web browser and the web server without 
+//reloading the entire web page.
+
+//onreadystatechange=>The onreadystatechange property in JavaScript is commonly used in conjunction with AJAX 
+//(Asynchronous //JavaScript and XML) to track the state changes of an XMLHttpRequest object.

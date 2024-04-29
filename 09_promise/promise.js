9fb371a54288718830217cdcf7d70a59746aa7cc
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-29 20:07:03 +0000

    promise and async await complete

--- 09_promise/promise.js
+++ 09_promise/promise.js
@@ -74,3 +74,53 @@ promiseFour
 .catch((error)=>{
     console.log(error)
 })
+.finally(()=>console.log("Finally complete"))
+
+//promise five with async await
+const promiseFive=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error=true
+        if(!error){
+            resolve({
+                username:"Shijal",
+                email:"shijal@123"
+            })
+        }else{
+            reject("Error: New error ooccured")
+        }
+    },1000)
+})
+async function newPromiseFIve(){
+    try {
+        const response=await promiseFive
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+newPromiseFIve()
+
+//fetching data from api using async and try catch
+async function newPromiseData(){
+    try{
+        const response=await fetch('https://jsonplaceholder.typicode.com/users')
+        const data=await response.json()
+        console.log(data)
+    }catch(error){
+        console.log(`${error}`)
+    }
+
+}
+newPromiseData()
+
+
+
+///fetching api from using promise
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((res)=>{
+    return res.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>console.log(`Error: ${error}`))
